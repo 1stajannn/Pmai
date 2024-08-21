2e361cddd6ab0396b790d0f425904757efc607cb
1stajannn <Pinyawee.tha@gmail.com>
2024-08-21 14:57:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import streamlit as st
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
+
 
 df = sns.load_dataset('iris')
 df
@@ -13,7 +17,8 @@ y = df['species']
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
 st.sidebar.title('Classifiers')
-classifier = st.sidebar.selectbox('Select Classifier', ('KNN', 'SVM'))
+classifier = st.sidebar.selectbox('Select Classifier', ('KNN', 'SVM', 'Decision Tree', 'Random Forest', 'Neural Network'))
+k = st.sidebar.slider('K', 1, 120, 3)
 if classifier == 'KNN':
   knn = KNeighborsClassifier(n_neighbors=3)
   knn.fit(x_train, y_train)
@@ -21,8 +26,26 @@ if classifier == 'KNN':
   acc = accuracy_score(y_test, y_pred)
   st.write(acc)
 if classifier == 'SVM':
-    svm = SVC()
-    svm.fit(x_train, y_train)
-    y_pred = svm.predict(x_test)
-    acc = accuracy_score(y_test, y_pred)
-    st.write(acc)
+  svm = SVC()
+  svm.fit(x_train, y_train)
+  y_pred = svm.predict(x_test)
+  acc = accuracy_score(y_test, y_pred)
+  st.write(acc)
+if classifier == 'Decision Tree'
+  dt = DecisionTreeClassifier()
+  dt.fit(x_train, y_train)
+  y_pred = dt.predict(x_test)
+  acc = accuracy_score(y_test, y_pred)
+  st.write(acc)
+if classifier == 'Random Forest'
+  rf = RandomForestClassifier()
+  rf.fit(x_train, y_train)
+  y_pred = rf.predict(x_test)
+  acc = accuracy_score(y_test, y_pred)
+  st.write(acc)
+if classifier == 'Neural Network'
+  nn = MLPClassifier()
+  nn.fit(x_train, y_train)
+  y_pred = nn.predict(x_test)
+  acc = accuracy_score(y_test, y_pred)
+  st.write(acc)
